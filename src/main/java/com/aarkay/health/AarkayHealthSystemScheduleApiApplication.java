c56ff8992fa06eb801cc7e97debd4c7e4360934d
rameshkatta <aarkatta@gmail.com>
2023-05-10 23:34:35 -0400

    Initial Commit

---
+++ src/main/java/com/aarkay/health/AarkayHealthSystemScheduleApiApplication.java
@@ -0,0 +1,13 @@
+package com.aarkay.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AarkayHealthSystemScheduleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AarkayHealthSystemScheduleApiApplication.class, args);
+    }
+
+}
